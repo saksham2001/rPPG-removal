c8b4b62bb15ac037bfd2d75ec5c04143ca4230a3
saksham2001 <sakshambhutani@Sakshams-MacBook-Pro.local>
2024-02-02 14:45:38 +0530

    added comments to writer

--- pyRemoval/utils/writer.py
+++ pyRemoval/utils/writer.py
@@ -8,28 +8,33 @@ License: MIT
 import os
 import xlsxwriter
 
-def save2excel(meth, mse, path, techniques):
+def save2excel(meth, path, metric, metric_name):
     '''
     This function saves the data to excel file.
 
     Parameters:
         meth (str): Method for removing rPPG signal.
-        mse (float): Mean Square Error.
         path (str): Path to save the excel file.
+        metric (float): Metric value.
+        metric_name (str): Name of the metric.
 
     Returns:
         None
     '''
+    # Create a workbook and add a worksheet.
     workbook = xlsxwriter.Workbook(os.path.join(path,f'img.xlsx'))
     worksheet = workbook.add_worksheet()
+
+    # Add headers
     row = 0
     col = 0
     worksheet.write(row, col, "Method")
-    worksheet.write(row, col + 1, "MSE")
+    worksheet.write(row, col + 1, metric_name)
 
+    # Write data
     row += 1
     for i in range(len(meth)):
         worksheet.write(row, col, meth[i])
-        worksheet.write(row, col + 1, mse[i])
+        worksheet.write(row, col + 1, metric[i])
         row += 1
     workbook.close()
