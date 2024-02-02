78582b7d0fb87196e022ae82ff2ae60775c15f8d
saksham2001 <sakshambhutani@Sakshams-MacBook-Pro.local>
2024-02-02 14:52:09 +0530

    added comments to extract

--- pyRemoval/processing/extract.py
+++ pyRemoval/processing/extract.py
@@ -26,6 +26,7 @@ def full_roi(frame, frame_height, frame_width):
         mask (numpy.ndarray): Mask of the frame.
     '''
 
+    # Create a mask of the same size as the frame
     return np.ones((frame_height, frame_width), np.uint8)
 
 def facial_roi(frame, frame_height, frame_width):
@@ -46,16 +47,21 @@ def facial_roi(frame, frame_height, frame_width):
             refine_landmarks=True,
             min_detection_confidence=0.5) as face_mesh:
         
+        # Process the frame to get the facial landmarks
         results = face_mesh.process(frame)
 
+        # If face is detected
         if results.multi_face_landmarks:
+            # Iterate over the detected faces
             for face_landmarks in results.multi_face_landmarks:
-
+                
+                # Get the list of facial landmarks
                 lmk_lst = np.array([np.array([int(pt.x*frame_width), int(pt.y*frame_height)]) for pt in face_landmarks.landmark])
 
-                # Get the facial region using Convex Hull
+                # Create a Convex Hull with the landmarks
                 hull = cv2.convexHull(lmk_lst)
 
+                # Create the mask using the Convex Hull
                 mask = np.zeros((frame_height, frame_width), np.uint8)
                 mask = cv2.fillConvexPoly(mask, hull, 255)
 
@@ -82,31 +88,38 @@ def selected_facial_roi(frame, frame_height, frame_width):
             refine_landmarks=True,
             min_detection_confidence=0.5) as face_mesh:
         
+        # Process the frame to get the facial landmarks
         results = face_mesh.process(frame)
 
+        # If face is detected
         if results.multi_face_landmarks:
+            # Iterate over the detected faces
             for face_landmarks in results.multi_face_landmarks:
 
-                landmarks = results.multi_face_landmarks[0].landmark
+                # Get the list of facial landmarks
+                landmarks = face_landmarks.landmark
 
                 # Get points for forehead, left cheek, and right cheek
                 forehead_points = np.array([(landmarks[lm_idx].x * frame_width, landmarks[lm_idx].y * frame_height) for lm_idx in forehead_lmk], np.int32)
                 left_cheek_points = np.array([(landmarks[lm_idx].x * frame_width, landmarks[lm_idx].y * frame_height) for lm_idx in left_cheek_lmk], np.int32)
                 right_cheek_points = np.array([(landmarks[lm_idx].x * frame_width, landmarks[lm_idx].y * frame_height) for lm_idx in right_cheek_lmk], np.int32)
 
-                # Get the facial region using Convex Hull
+                # Get the facial regions using Convex Hull
                 hull_forehead = cv2.convexHull(forehead_points)
                 hull_cheek_left = cv2.convexHull(left_cheek_points)
                 hull_cheek_right = cv2.convexHull(right_cheek_points)
 
+                # Create masks for each facial region
                 mask_forehead = np.zeros((frame_height, frame_width), np.uint8)
                 mask_cheek_left = np.zeros((frame_height, frame_width), np.uint8)
                 mask_cheek_right = np.zeros((frame_height, frame_width), np.uint8)
 
+                # Fill the masks with the facial regions
                 mask_forehead = cv2.fillConvexPoly(mask_forehead, hull_forehead, 255)
                 mask_cheek_left = cv2.fillConvexPoly(mask_cheek_left, hull_cheek_left, 255)
                 mask_cheek_right = cv2.fillConvexPoly(mask_cheek_right, hull_cheek_right, 255)
 
+                # Combine the masks
                 mask = mask_forehead + mask_cheek_left + mask_cheek_right
 
                 return mask
@@ -128,6 +141,8 @@ def boilerplate_roi(frame, frame_height, frame_width):
     '''
 
     # find new roi here and return mask. 
-    # The mask should have 255 pixel values for the pixels that need to be edited by the filter and 0 pixel values for the pixels that need to be left unedited.
+    
+    # The mask should have 255 pixel values for the pixels that need to be edited by the filter and 0 pixel values 
+    # for the pixels that need to be left unedited.
 
     pass
