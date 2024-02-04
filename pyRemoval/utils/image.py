86ba548d63788d321f505c1f194f4ae4d8836bf9
saksham2001 <sakshambhutani@Sakshams-MacBook-Pro.local>
2024-02-05 05:07:42 +0530

    added tqdm bar description

--- pyRemoval/utils/image.py
+++ pyRemoval/utils/image.py
@@ -25,7 +25,7 @@ def compute_errors(path1, path2, metric, notebook_mode=False):
     cap1 = cv2.VideoCapture(path1)
     cap2 = cv2.VideoCapture(path2)
     
-    mse_lst = []
+    metric_last = []
     
     vid1_length = int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
     vid2_length = int(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -51,10 +51,11 @@ def compute_errors(path1, path2, metric, notebook_mode=False):
             ret2, frame2 = cap2.read()
             
             if ret1 and ret2:
-                mse_val = metric(frame1, frame2)
+                metric_val = metric(frame1, frame2)
 
-                mse_lst.append(mse_val)
+                metric_last.append(metric_val)
                 pbar.update(1)
+                pbar.set_description('Metric Current: {:.2f} | Avg: {:.2f}'.format(metric_val, np.mean(metric_last)))
             # if no frame found
             else:
                 break
@@ -62,5 +63,5 @@ def compute_errors(path1, path2, metric, notebook_mode=False):
     cap1.release()
     cap2.release()
     
-    # return np.mean(mse_lst), np.mean(ssim_lst)
-    return np.mean(mse_lst)
+    # return np.mean(metric_last), np.mean(ssim_lst)
+    return np.mean(metric_last)
